73916836394d903a6ef12461964565fa1bf8a525
FuryFR <naelduval@gmail.com>
2023-09-24 19:35:15 -0700

    final version

--- test.js
+++
@@ -1,5 +0,0 @@
-const { generateResponse } = require('./server'); // Adjust the path as needed
-
-// Test cases for generateResponse function
-console.log(generateResponse('value1'));
-console.log(generateResponse('value2'));
